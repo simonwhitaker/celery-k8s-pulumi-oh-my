446340810c41c5d3b21893e603f7653db4ec14b8
Simon Whitaker <sw@netcetera.org>
2025-12-21 06:57:29 +0000

    Declare tailscale namespace explicitly

--- infrastructure/resources/tailscale.py
+++ infrastructure/resources/tailscale.py
@@ -1,10 +1,16 @@
 import pulumi
+from pulumi_kubernetes.core.v1 import Namespace
 from pulumi_kubernetes.helm.v3 import Release, ReleaseArgs, RepositoryOptsArgs
+from pulumi_kubernetes.meta.v1 import ObjectMetaArgs
 
 tailscale_config = pulumi.Config("tailscale")
 client_id = tailscale_config.require("clientId")
 client_secret = tailscale_config.require_secret("clientSecret")
 
+tailscale_namespace = Namespace(
+    "tailscale-namespace",
+    metadata=ObjectMetaArgs(name="tailscale"),
+)
 
 tailscale_operator = Release(
     "tailscale-operator",
@@ -14,8 +20,7 @@ tailscale_operator = Release(
             repo="https://pkgs.tailscale.com/helmcharts",
         ),
         version="1.92.4",
-        namespace="tailscale",
-        create_namespace=True,
+        namespace=tailscale_namespace.metadata.name,
         values={
             "oauth": {
                 "clientId": client_id,
